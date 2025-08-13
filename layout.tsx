132aa8357f66f6205aee0ed7c70e0501c5022d23
lev0ni <131240144+lev0ni@users.noreply.github.com>
2025-08-13 20:50:59 +0300

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,10 @@
+
+import './globals.css';
+export const metadata = { title: 'ArnoldOz' };
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ru">
+      <body><div className="wrapper">{children}</div></body>
+    </html>
+  );
+}
---
+++ page.tsx
@@ -0,0 +1,12 @@
+
+'use client';
+import Screen from './components/Screen';
+import MainSVG from '../design/svg/main-screen.svg';
+
+export default function Page(){
+  return (
+    <Screen map={{ 'btn-crm':'/crm/users', 'btn-callcenter':'/callcenter', 'btn-home':'/' }}>
+      <MainSVG />
+    </Screen>
+  );
+}
